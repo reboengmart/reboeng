16cd88607e2310a827eeb750ba8314560cbf387a
fachruwildan <fachruwildan1@gmail.com>
2020-07-01 13:36:04 +0700

    menampilkan Category berdasarkan database

--- lib/services/api/productCategory_api.dart
+++ lib/services/api/productCategory_api.dart
@@ -1,18 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:reboeng/provider/ProductCategoryNotifier.dart';
 import 'package:reboeng/services/model/ProductCategory.dart';
-
-getProductCategory(ProductCategoryNotifier productCategoryNotifier)async{
-  QuerySnapshot snapshot = await Firestore.instance
-      .collection('category')
-      .getDocuments();
-
-  List<ProductCategory> _productCategoryList= [];
-
-  snapshot.documents.forEach((document) {
-    ProductCategory productCategory = ProductCategory.fromMap(document.data);
-    _productCategoryList.add(productCategory);
-  });
-
-  productCategoryNotifier.productCategoryList = _productCategoryList;
+class GetProductCategory {
+  Firestore _db = Firestore.instance;
+   Stream<List<ProductCategory>> getCategory(){
+    return  _db.collection('category').snapshots().map((snapshot) => snapshot.documents.map((document)=> ProductCategory.fromFirestore(document.data)).toList());
+   }
 }
--- lib/ui/Screens/Home/components/category_grid.dart
+++ lib/ui/Screens/Home/components/category_grid.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:reboeng/provider/ProductCategoryNotifier.dart';
+import 'package:reboeng/services/model/ProductCategory.dart';
 import 'package:reboeng/ui/Screens/Product/productlist.dart';
 
 class CategoryGrid extends StatelessWidget {
@@ -11,6 +13,7 @@ class CategoryGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
 //    final categoryList = [
 //      {
 //        'nama': 'Sayur',
@@ -53,41 +56,39 @@ class CategoryGrid extends StatelessWidget {
 //        'status': 'comingsoon'
 //      },
 //    ];
-
+    final categorys = Provider.of<List<ProductCategory>>(context);
       return Container(
         padding: EdgeInsets.only(left: 15.0, right: 15.0),
         alignment: Alignment.center,
-        child: Consumer<ProductCategoryNotifier>(
-          builder: (context, category, _ ) => GridView.count(
+        child: (categorys !=null) ? GridView.count(
             physics: new NeverScrollableScrollPhysics(),
             primary: false,
-            padding: const EdgeInsets.all(0),
+            padding:  const EdgeInsets.all(0),
             crossAxisSpacing: 0,
             mainAxisSpacing: 15,
             crossAxisCount: 4,
-            children: List.generate(category.productCategoryList.length, (index) {
-              final categoryData = category.productCategoryList[index];
-              return InkWell(
-                  child: Column(
-                    children: <Widget>[
-                      Image(
-                        image: AssetImage(categoryData.assets),
-                        fit: BoxFit.fitHeight,
-                      ),
-                    ],
-                  ),
-                  onTap: () {
-                    (categoryData.status == 'available') ? Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProductList(categoryData.nama),
-                        )
-                    ) : 0;
-                  }
-              );
-            }),
-          ),
-        ),
+          children: List.generate(categorys.length, (index) {
+            final categoryData = categorys[index];
+            return InkWell(
+                child: Column(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage(categoryData.assets),
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ],
+                ),
+                onTap: () {
+                  (categoryData.status == 'available') ? Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductList(categoryData.nama),
+                      )
+                  ) : 0;
+                }
+            );
+          }),
+        ):Center(child: CircularProgressIndicator())
       );
     }
   }
