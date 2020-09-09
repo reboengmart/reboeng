434f913cf2e97dacbf2bfb3f0b395373d2a27dd0
reboengmart <reboengmart@gmail.com>
2020-09-09 13:43:51 +0700

    Update AddressMainScreen

--- lib/services/model/address.dart
+++ lib/services/model/address.dart
@@ -3,16 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Address{
   String id;
   String nama;
-  bool notSelected;
   GeoPoint geo;
   String detail;
   String icon;
   String status;
-  Address({this.id, this.nama,this.notSelected,this.geo,this.detail,this.icon,this.status});
+  Address({this.id, this.nama,this.geo,this.detail,this.icon,this.status});
   Address.formMap(Map<String,dynamic> data){
     id=data['id'];
     nama=data['nama'];
-    notSelected=data['notSelected'];
     geo=data['geo'];
     detail=data['detail'];
     icon=data['icon'];
@@ -22,7 +20,6 @@ class Address{
     return {
       'id': id,
       'nama': nama,
-      'notSelected': notSelected,
       'geo': geo,
       'detail': detail,
       'icon': icon,
