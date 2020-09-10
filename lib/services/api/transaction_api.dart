f5bdce396f547ac9702c29a6136684d18255b949
fachruwildan <fachruwildan1@gmail.com>
2020-09-10 17:44:23 +0700

    delete cart

--- lib/services/api/transaction_api.dart
+++ lib/services/api/transaction_api.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:reboeng/services/model/Cart.dart';
 import 'package:reboeng/services/model/InvoiceModel.dart';
 import 'package:reboeng/services/model/TransactionModel.dart';
 
@@ -16,6 +18,7 @@ class TransactionApi{
           transactionModel.id_transaction).setData(transactionModel.toMap());
        Firestore.instance.collection('transaction').document(transactionModel.id_transaction).collection('invoice').document(
           invoiceModel.id_invoice).setData(invoiceModel.toMap());
+       deletecart();
        return Firestore.instance.collection('transaction').document(transactionModel.id_transaction).collection('invoice').document(invoiceModel.id_invoice).setData({'address' : invoiceModel.address, 'date_invoice' : invoiceModel.date_invoice, 'delivery': invoiceModel.delivery, 'id_invoice' : invoiceModel.id_invoice, 'total_invoice' : invoiceModel.total_invoice, 'total_payment' : invoiceModel.total_payment, 'user_ref' : invoiceModel.user_ref, 'detail_invoice' : FieldValue.arrayUnion(invoiceModel.DetailInvoice.map((e) => e.toMap()).toList())});
     }else{
       snapshot.documents.forEach((element) {
@@ -27,9 +30,33 @@ class TransactionApi{
           .document(_listTransaction.first.id_transaction)
           .updateData({'total_transaction': _listTransaction.first.total_transaction + invoiceModel.total_payment});
       print(invoiceModel.DetailInvoice.map((e) => e.toMap()).toString());
+      deletecart();
       return Firestore.instance.collection('transaction').document(_listTransaction.first.id_transaction).collection('invoice').document(invoiceModel.id_invoice).setData({'address' : invoiceModel.address, 'date_invoice' : invoiceModel.date_invoice, 'delivery': invoiceModel.delivery, 'id_invoice' : invoiceModel.id_invoice, 'total_invoice' : invoiceModel.total_invoice, 'total_payment' : invoiceModel.total_payment, 'user_ref' : invoiceModel.user_ref, 'detail_invoice' : FieldValue.arrayUnion(invoiceModel.DetailInvoice.map((e) => e.toMap()).toList())});
     }
 
+  }
+   Future<void> deletecart() async {
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final FirebaseUser user = await _auth.currentUser();
+    String uid = user.uid;
+    QuerySnapshot snapshot2 = await Firestore.instance.collection('user').document('${uid}').collection('cart').getDocuments();
+   return snapshot2.documents.forEach((element) {
+      Firestore.instance
+        .collection('user')
+        .document('${uid}')
+        .collection('cart')
+        .document(element.documentID)
+        .delete();
+//      Cart cart= Cart.formMap(element.data);
+////      _cartMap.addAll(detaiInvoiceModel.toMap());
+//      _array.add(cart);
+    });
+//    return Firestore.instance
+//        .collection('user')
+//        .document(uid)
+//        .collection('cart')
+//        .document(id)
+//        .delete();
   }
 //  Future<void> saveCart(TransactionModel transactionModel,InvoiceModel invoiceModel,List liscart) async{
 //    DocumentReference docRef=Firestore.instance.collection('transaction').document(transactionModel.id_transaction).collection('invoice').document(invoiceModel.id_invoice);
